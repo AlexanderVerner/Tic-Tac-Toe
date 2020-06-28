2994516c0480d32d2f73aebb292d840a7af92e94
Alexander Verner <Verner.Alx@gmail.com>
2020-06-28 18:00:57 +0500

    Initial Commit

---
+++ Tic-Tac-Toe/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tic-Tac-Toe
+//
+//  Created by Александр Терентьев on 28.06.2020.
+//  Copyright © 2020 AlexanderVerner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
