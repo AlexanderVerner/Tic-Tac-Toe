b1b275c44cf9209de9cba36cb2b22a0021bfc34d
Alexander Verner <Verner.Alx@gmail.com>
2020-06-28 20:29:20 +0500

    Add navigation bar

--- Tic-Tac-Toe/Drow.swift
+++ Tic-Tac-Toe/Drow.swift
@@ -9,16 +9,20 @@
 import Foundation
 
 func Draw() {
-    var row0 = " \(fieldArr[0][0]) | \(fieldArr[0][1]) | \(fieldArr[0][2]) "
-    var row1 = " \(fieldArr[1][0]) | \(fieldArr[1][1]) | \(fieldArr[1][2]) "
-    var row2 = " \(fieldArr[2][0]) | \(fieldArr[2][1]) | \(fieldArr[2][2]) "
-    let separators = "---+---+---"
+    var row0 = "{0}  \(fieldArr[0][0]) | \(fieldArr[0][1]) | \(fieldArr[0][2]) "
+    var row1 = "{1}  \(fieldArr[1][0]) | \(fieldArr[1][1]) | \(fieldArr[1][2]) "
+    var row2 = "{2}  \(fieldArr[2][0]) | \(fieldArr[2][1]) | \(fieldArr[2][2]) "
+    let separators = "    ---+---+---"
+    let navigationHelper = "    {0}|{1}|{2}"
     print("""
+            \(navigationHelper)
+
             \(row0)
             \(separators)
             \(row1)
             \(separators)
             \(row2)
+
           """)
-    print("****************")
+    print("  ******************")
 }
