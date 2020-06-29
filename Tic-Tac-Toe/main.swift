39068905e393bea5e13e301b5358f4e88cf91490
Alexander Verner <Verner.Alx@gmail.com>
2020-06-29 23:46:42 +0500

    Now it is possible to set "x" on the playing field for first player

--- Tic-Tac-Toe/main.swift
+++ Tic-Tac-Toe/main.swift
@@ -9,13 +9,36 @@
 import Foundation
 //Create array for game field
 var fieldArr = Array(repeating: Array(repeating: " ", count: 3), count:  3)
-var coordinates = (row: 0, column: 0)
+var gameOn: Bool = true
+var row:Int = 0
+var col:Int = 0
+//var coordinates = Array(arrayLiteral: 0, 0)
+//var coord1: String? = Array()
+let messagesEn = [
+"Welcome to the game Tic Tac Toe. Good game!",
+"What is the name of the first player?",
+"What is the name of the second player?"
+]
 //Initial message
 print("""
- Welcome to the game Tic Tac Toe. Good game!
+\(messagesEn[0])
 
+  ******************
 """)
-Draw()
+print(messagesEn[1])
+var firstPlayer: String? = readLine()
+//print(messagesEn[2])
+//var secondPlayer: String? = readLine()
+repeat {
+    Draw()
+    print("Move player \(firstPlayer!)")
+    var coordinates = readLine()?.split {$0 == ","}.map (String.init)
+    row = Int(coordinates![0])!
+    col = Int(coordinates![1])!
+    fieldArr[row][col] = "X"
+    Draw()
+    gameOn = false
+} while gameOn == true
 //fieldArr[1][1] = "X"
 //Draw()
 //fieldArr[0][0] = "0"
